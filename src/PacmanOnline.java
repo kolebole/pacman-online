28bc38644e1ff8522a577dbf62e1651491b34315
rexkimta <rexkimta@f44405b4-e753-f845-00da-4f68cdd347b1>
2010-06-07 11:21:02 +0000

    Make pacframe

--- src/PacmanOnline.java
+++ src/PacmanOnline.java
@@ -1,3 +1,5 @@
+import javax.swing.SwingUtilities;
+
 /* File: PacmanOnline.java
  * Start: 2010/06/07
  * Modification: 2010/06/XX
@@ -11,6 +13,13 @@ public class PacmanOnline {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, Pacman-Online!");
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				PacFrame inst = new PacFrame();
+				inst.setLocationRelativeTo(null);
+				inst.setVisible(true);
+			}
+		});
 	}
 
 }

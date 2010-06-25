9500525c6d0444942b8004d88598ea167b52b669
ryanlei750328 <ryanlei750328@f44405b4-e753-f845-00da-4f68cdd347b1>
2010-06-25 09:50:37 +0000

    Add an interface Constants for defining constants.

---
+++ src/Constants.java
@@ -0,0 +1,24 @@
+/* File: template.java
+ * Start: 2010/06/25
+ * Modification: 2010/06/XX
+ * Description: Constants used in this game.
+ */
+
+public interface Constants {
+	/*** Game-related ***/
+	/* maximum allowable number of players */
+	public static final int MAX_PLAYERS = 8;
+	
+	/*** GUI-related ***/
+	/* The whole frame (window) */
+	public final int FRAME_WIDTH = 1000;
+	public final int FRAME_HEIGHT = 650;
+	/* The game panel */
+	public static final int GAME_WIDTH = 700;
+	public static final int GAME_HEIGHT = 500;
+	
+	/*** Network-related ***/
+	/* This game uses the constant port port 4233 */
+	public static final int port = 4233;
+
+}

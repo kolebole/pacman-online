b81608d2254f73689689ccb500e9244f84b358c7
ryanlei750328 <ryanlei750328@f44405b4-e753-f845-00da-4f68cdd347b1>
2010-06-25 10:29:26 +0000

    Restrict max number of players

--- src/ServerThread.java
+++ src/ServerThread.java
@@ -7,39 +7,39 @@ import javax.swing.*;
  *              This thread is to avoid I/O blocking.
  */
 
-public class ServerThread implements Runnable {
+public class ServerThread implements Constants, Runnable {
 	ServerSocket ss;
 	Socket cs;
 	private static final int port = ConnectPanel.port;
 	JTextField nickField;
 	JTextField addrField;
 	JButton serverButton;
+	public int numPlayers;
 	
 	/* constructor */
-	ServerThread(JTextField nickField, JTextField addrField, JButton serverButton) {
-		this.nickField = nickField;
-		this.addrField = addrField;
-		this.serverButton = serverButton;
+	ServerThread() {
 		Thread thread = new Thread(this);
 		thread.start();
 	}
 	
 	@Override
+	/* Entry point when a new thread is created */
 	public void run() {
 		System.out.println("Server: create a thread.");
 		try {
 			ss = new ServerSocket(port);
 			System.out.println("Server: Listen on port " + port + " ...");
-			/* disable other textfields or buttons */
-			nickField.setEnabled(false);
-			addrField.setEnabled(false);
-			serverButton.setEnabled(false);
-			
-			while (true) {
+			numPlayers = 1;
+			System.out.println("Number of players: " + numPlayers);
+
+			/* Always wait for clients when ... # of players < 8 ?? */
+			while (numPlayers < MAX_PLAYERS) {
 				cs = ss.accept();
-				System.out.println("Server: Socket accepted.");
-				System.out.println("Server: Client from " + cs.getInetAddress());
+				numPlayers++;
+				System.out.println("Server: Client from " + cs.getInetAddress() + ":" + cs.getPort());
+				System.out.println("Number of players: " + numPlayers);
 			}
+			System.out.println("The room is full !");
 
 		}
 		catch (Exception e) {

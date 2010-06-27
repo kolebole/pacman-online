506c8ba2ff867a64555545340c74a7db4e6a4824
rexkimta <rexkimta@f44405b4-e753-f845-00da-4f68cdd347b1>
2010-06-27 08:56:05 +0000

    Server could controll what character they choose.

--- trunk/src/DirectionListener.java
+++ trunk/src/DirectionListener.java
@@ -14,49 +14,28 @@ public class DirectionListener implements KeyListener {
 	@Override
 	public void keyPressed(KeyEvent e) {
 		int keyCode = e.getKeyCode();
+		char team = TeamManager.vector.get(0).team;
+		int number = TeamManager.vector.get(0).picIndex;
+		Player player = playerList[team*4+number];
 		switch( keyCode ){
 		case KeyEvent.VK_UP:
-			playerList[0].newDirect = KeyEvent.VK_UP;
-/*			playerList[1].direction = KeyEvent.VK_UP;
-			playerList[1].changeDir(KeyEvent.VK_UP);
-			playerList[2].direction = KeyEvent.VK_UP;
-			playerList[2].changeDir(KeyEvent.VK_UP);
-			playerList[3].direction = KeyEvent.VK_UP;
-			playerList[3].changeDir(KeyEvent.VK_UP);*/
+			player.newDirect = KeyEvent.VK_UP;
 			break;
 		case KeyEvent.VK_DOWN:
-			playerList[0].newDirect = KeyEvent.VK_DOWN;
-/*			playerList[1].direction = KeyEvent.VK_DOWN;
-			playerList[1].changeDir(KeyEvent.VK_DOWN);
-			playerList[2].direction = KeyEvent.VK_DOWN;
-			playerList[2].changeDir(KeyEvent.VK_DOWN);
-			playerList[3].direction = KeyEvent.VK_DOWN;
-			playerList[3].changeDir(KeyEvent.VK_DOWN);*/
+			player.newDirect = KeyEvent.VK_DOWN;
 			break;
 		case KeyEvent.VK_LEFT:
-			playerList[0].newDirect = KeyEvent.VK_LEFT;
-/*			playerList[1].direction = KeyEvent.VK_LEFT;
-			playerList[1].changeDir(KeyEvent.VK_LEFT);
-			playerList[2].direction = KeyEvent.VK_LEFT;
-			playerList[2].changeDir(KeyEvent.VK_LEFT);
-			playerList[3].direction = KeyEvent.VK_LEFT;
-			playerList[3].changeDir(KeyEvent.VK_LEFT);*/
+			player.newDirect = KeyEvent.VK_LEFT;
 			break;
 		case KeyEvent.VK_RIGHT:
-			playerList[0].newDirect = KeyEvent.VK_RIGHT;
-/*			playerList[1].direction = KeyEvent.VK_RIGHT;
-			playerList[1].changeDir(KeyEvent.VK_RIGHT);
-			playerList[2].direction = KeyEvent.VK_RIGHT;
-			playerList[2].changeDir(KeyEvent.VK_RIGHT);
-			playerList[3].direction = KeyEvent.VK_RIGHT;
-			playerList[3].changeDir(KeyEvent.VK_RIGHT);*/
+			player.newDirect = KeyEvent.VK_RIGHT;
 			break;
 
 		default:
 			System.out.println("KeyCode is " + keyCode );
 		}
-		System.out.println("Player move to: " + playerList[0].x + ", " + playerList[0].y);
-		System.out.println("Player move to: " + playerList[0].gridx + ", " + playerList[0].gridy);
+		System.out.println("Player move to: " + player.x + ", " + player.y);
+		System.out.println("Player move to: " + player.gridx + ", " + player.gridy);
 		//frame.movePlayer(playerList);
 	}
 
